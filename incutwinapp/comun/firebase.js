ec75c614a2791e9467847a43cbdd0f99f53dbb7b
jlacostaarpide <jlacostaarp@alumni.unav.es>
2026-05-08 16:32:06 +0200

    Persistence AsyncStorage of firebase

--- incutwinapp/comun/firebase.js
+++ incutwinapp/comun/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyANwzDWwczsHJgOa3EDcAoLpTVrv7YvHYI',
@@ -12,4 +13,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+});
